2d6a1e392693fdd4afc3bdc676b70d0c211d9d1d
James Schulz <jamesschulzm@gmail.com>
2025-11-11 19:20:19 -0300

    [ADD] Criando o dao com floor

--- lib/data/dao/database.dart
+++ lib/data/dao/database.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
-
 import 'package:floor/floor.dart';
+import 'package:hyrule/data/dao/entry_dao.dart';
 import 'package:hyrule/domain/models/entry.dart';
-import 'package:sqflite/sqflite.dart' as sqflite;
 
 part 'database.g.dart';
 
