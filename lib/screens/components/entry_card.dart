0091ddfaedef98dfe0a6f4a75654340c82c577b1
James Schulz <jamesschulzm@gmail.com>
2025-11-27 19:15:46 -0300

    [ADD] Construindo o cartão de entrada

---
+++ lib/screens/components/entry_card.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:hyrule/domain/models/entry.dart';
+
+class EntryCard extends StatelessWidget {
+  final Entry entry;
+
+  const EntryCard({Key? key, required this.entry}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          InkWell(
+            onTap: () {},
+            child: Ink(
+              child: Row(
+                children: <Widget>[
+                  Image.network(entry.image),
+                  Column(
+                    children: <Widget>[
+                      Text(entry.name),
+                      Text(entry.description),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Wrap(
+            children: entry
+                .commonLocationsConverter()
+                .map((e) => Chip(
+                      label: Text(e),
+                    ))
+                .toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
